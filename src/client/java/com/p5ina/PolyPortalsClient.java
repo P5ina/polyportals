3b234d07cdb4bba2426e6d4af6a3f75683a899f0
Timur Turatbekov <timatur242005@gmail.com>
2024-01-24 02:10:54 +0300

    Initial project

---
+++ src/client/java/com/p5ina/PolyPortalsClient.java
@@ -0,0 +1,10 @@
+package com.p5ina;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PolyPortalsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
